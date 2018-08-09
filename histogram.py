c2d2097b105d65dde665da0efb420c420bda635a
Donovan Adams <donovan.adams@students.makeschool.com>
2018-08-09 14:51:33 -0700

    starting histogrampages

---
+++ histogram.py
@@ -0,0 +1,15 @@
+
+class Histogram:
+    "This Class makes a histogram out of a "
+    def __init__(self,file_str):
+        file = open(file_str,'r')
+        file_list = file.readlines()
+
+    def listogram(self,file_list):
+        pass
+    def dictogram(self,file_list):
+        pass
+    def tuplegram(self,file_list):
+        pass
+if __name__ == '__main__':
+    main()
